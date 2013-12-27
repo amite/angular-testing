454d959bcbbd49b7a620253c6674eeeedd86714a
amite <amit.erandole@gmail.com>
2013-12-27 09:40:18 +0530

    added the CalcDefaults values object

--- app/calc.js
+++ app/calc.js
@@ -1,5 +1,20 @@
 var ptCalc = angular.module('PT-Calculator', []);
 
+ptCalc.value('CalcDefaults', {
+  storage: 10,
+  noServers: 1,
+  
+  wkBackups: 1,
+  yrBackups: 1,
+  mthBackups: 1,
+  dlBackups: 1,
+  
+  yrChange: 0.05,
+  dlChange: 0.01,
+
+  compressionRate: 0.4
+});
+
 ptCalc.controller('PtCalcCtrl', function($scope) {
   $scope.outsourced = {
     status: "Outsourced"
--- test/calc-spec.js
+++ test/calc-spec.js
@@ -9,7 +9,48 @@ describe("PtCalcCtrl", function() {
       $rootScope = $injector.get('$rootScope');
       $scope = $rootScope.$new();
       ctrl = $injector.get('$controller')("PtCalcCtrl", {$scope: $scope});
+      defaults = $injector.get('CalcDefaults');
     });
+  });
+
+  describe('Setting Calculator defaults', function () {
+    
+    it("should instantiate with storage of 10 GB", function() {
+      expect(defaults.storage).toEqual(10);
+    });
+
+    it("should instantiate with 1 server", function() {
+      expect(defaults.noServers).toEqual(1);
+    });
+
+    it("should instantiate with 1 weekly Backup", function() {
+      expect(defaults.wkBackups).toEqual(1);
+    });
+
+    it("should instantiate with 1 yearly Backup", function() {
+      expect(defaults.yrBackups).toEqual(1);
+    });
+
+    it("should instantiate with 1 monthly Backup", function() {
+      expect(defaults.mthBackups).toEqual(1);
+    });
+
+    it("should instantiate with 1 daily Backup", function() {
+      expect(defaults.dlBackups).toEqual(1);
+    });
+
+    it("should instantiate with a yearly change rate of 0.05", function() {
+      expect(defaults.yrChange).toEqual(0.05);
+    });
+
+    it("should instantiate with a daily change rate of 0.01", function() {
+      expect(defaults.dlChange).toEqual(0.01);
+    });
+
+    it("should instantiate with a compression rate of 0.4", function() {
+      expect(defaults.compressionRate).toEqual(0.4);
+    });
+
   });
 
   describe("Initialization", function() {
